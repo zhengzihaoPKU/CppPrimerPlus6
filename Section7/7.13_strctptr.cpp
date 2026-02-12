2f9665bcbe05b0d94b5e355377b51816ce434665
hangingpot-zzh <2201210721@stu.pku.edu.cn>
2026-02-12 12:00:23 +0800

    revise

--- Section7/7.13_strctptr.cpp
+++ Section7/7.13_strctptr.cpp
@@ -22,6 +22,24 @@ int main(){
     polar pplace;
     cout << "Enter the x and y values: ";
     while(cin >> rplace.x >> rplace.y){
-        
+        rect_to_polar(&rplace, &pplace);
+        show_polar(&pplace);
+        cout << "Next two numbers (q to quit): ";
     }
+    cout << "Done.\n";
+    return 0;
+}
+
+void show_polar(const polar *pda){
+    using namespace std;
+    const double Rad_to_deg = 57.29577951;
+    cout << "distance = " << pda->distance;
+    cout << ", angle = " << pda->angle * Rad_to_deg;
+    cout << " degrees\n";
+}
+
+void rect_to_polar(const rect *pxy, polar *pda){
+    using namespace std;
+    pda->distance = sqrt(pxy->x * pxy->x + pxy->y + pxy->y);
+    pda->angle = atan2(pxy->y, pxy->x);
 }
