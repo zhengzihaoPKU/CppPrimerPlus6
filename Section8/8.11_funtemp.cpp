99cf7c9e171729a38eeef147fdd772b23e67f94e
hangingpot-zzh <2201210721@stu.pku.edu.cn>
2026-02-17 00:32:34 +0800

    revise

---
+++ Section8/8.11_funtemp.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+template <typename T> // or class T
+void Swap(T &a, T &b);
+
+int main(){
+    using namespace std;
+    int i = 10;
+    int j = 20;
+    cout << "i, j = " << i << ", " << j << ".\n";
+    cout << "Using compiler generated int swapper:\n";
+    Swap(i, j); // generated void Swap(int &, int &);
+    cout << "Now i, j = " << i << ", " << j << ".\n";
+
+    double x = 24.5;
+    double y = 81.7;
+    cout << "x, y = " << x << ", " << y << ".\n";
+    Swap(x, y); // generates void Swap(double &, double &)
+    cout << "Now x, y = " << x << ", " << y << ".\n";
+    return 0;
+}
+
+template <typename T> // or class
+void Swap(T &a, T &b){
+    T temp; // temp a variable of type T
+    temp = a;
+    a = b;
+    b = temp;
+}
