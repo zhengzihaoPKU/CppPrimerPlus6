c343903eadc0d83eb8dc5360833860bf3f869f29
hangingpot-zzh <2201210721@stu.pku.edu.cn>
2026-02-17 22:20:06 +0800

    revise

---
+++ Section8/8.15_choices.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+template<class T> // #1
+T lesser(T a, T b){
+    return a < b ? a : b;
+}
+
+int lesser(int a, int b){ // #2
+    a = a < 0 ? -a : a;
+    b = b < 0 ? -b : b;
+    return a < b ? a : b;
+}
+
+int main(){
+    using namespace std;
+    int m = 20;
+    int n = -30;
+    double x = 15.5;
+    double y = 25.9;
+
+    cout << lesser(m, n) << endl; // use #2
+    cout << lesser(x, y) << endl; // use #1 with double
+    cout << lesser<>(m, n) << endl; // use #1 with int
+    cout << lesser<int>(x,y) << endl; // use #1 with int, transfer double into int
+    return 0;
+}
