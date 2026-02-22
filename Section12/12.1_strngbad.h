d3f9374b3f198d0570ab42af4803d94652823e20
hangingpot-zzh <2201210721@stu.pku.edu.cn>
2026-02-23 00:35:25 +0800

    revise

---
+++ Section12/12.1_strngbad.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#ifndef STRNGBAD_H_
+#define STRNGBAD_H_
+
+class StringBad
+{
+    private:
+        char * str; // pointer to string
+        int len; // length of string
+        static int num_strings; // number of objects
+    public:
+        StringBad(const char *s); // constructor
+        StringBad(); // default constructor
+        // friend function
+        friend std::ostream & operator<<(std::ostream & os, const StringBad & st);
+};
+
+#endif
