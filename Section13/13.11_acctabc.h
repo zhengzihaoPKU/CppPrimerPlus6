498f2e43d3b21076666343fc2d8c8ce0ea927c3b
hangingpot-zzh <2201210721@stu.pku.edu.cn>
2026-02-25 18:37:25 +0800

    revise

---
+++ Section13/13.11_acctabc.h
@@ -0,0 +1,76 @@
+#ifndef ACCTABC_H_
+#define ACCTABC_H_
+#include<iostream>
+#include<string>
+
+// abstract Base class
+class AcctABC
+{
+    private:
+        std::string fullName;
+        long acctNum;
+        double balance;
+    protected:
+        struct Formatting
+        {
+            std::ios_base::fmtflags flag;
+            std::streamsize pr;
+        };
+        const std::string & FullName() const
+        {
+            return fullName;
+        }
+        long AcctNum() const 
+        {
+            return acctNum;
+        }
+        Formatting SetFormat() const;
+        void Restore(Formatting & f) const;
+    public:
+        AcctABC(const std::string & s = "Nullbody", long an = -1, double bal = 0.0);
+        void Deposit(double = 0.0);
+        virtual void Withdraw(double amt) = 0; // pure virtual function
+        double Balance() const
+        {
+            return balance;
+        }
+        virtual void ViewAcct() const = 0; // pure virtual function
+        virtual ~AcctABC(){}
+};
+
+// Brass Account Class
+class Brass : public AcctABC
+{
+    public:
+        Brass(const std::string & s = "Nullbody", long an = -1, double bal = 0.0) : AcctABC(s, an, bal){}
+        virtual void ViewAcct() const;
+        virtual void Withdraw(double amt);
+        virtual ~Brass(){}
+};
+
+// Brass Plus Account Class
+class BrassPlus : public AcctABC
+{
+    private:
+        double maxLoan;
+        double rate;
+        double owesBank;
+    public:
+        BrassPlus(const std::string & s = "Nullbody", long an = -1, double bal = 0.0, double ml = 500, double r = 0.10);
+        BrassPlus(const Brass & ba, double ml = 500, double r = 0.1);
+        virtual void ViewAcct() const;
+        virtual void Withdraw(double amt);
+        void ResetMax(double m)
+        {
+            maxLoan = m;
+        }
+        void ResetRate(double r)
+        {
+            rate = r;
+        }
+        void ReserOwes()
+        {
+            owesBank = 0;
+        }
+};
+#endif
